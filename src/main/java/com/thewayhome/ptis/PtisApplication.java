4286c725662b8e91ca45ecb27416efad697f2c5b
wasuphj <ninini6630@gmail.com>
2023-10-14 15:36:50 +0900

    [KHJ] 프로젝트 오픈

---
+++ src/main/java/com/thewayhome/ptis/PtisApplication.java
@@ -0,0 +1,13 @@
+package com.thewayhome.ptis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thewayhome/ptis/PtisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thewayhome.ptis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
