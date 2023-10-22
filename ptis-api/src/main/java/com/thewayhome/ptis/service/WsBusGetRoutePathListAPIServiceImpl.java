504ca7c113599a834d0da345ab0539d60e27ee03
wasuphj <ninini6630@gmail.com>
2023-10-22 12:09:42 +0900

    [KHJ] 서울시 버스 노선 지도상 경로 조회 서비스 API 구현 - 서비스 구현

---
+++ ptis-api/src/main/java/com/thewayhome/ptis/service/WsBusGetRoutePathListAPIServiceImpl.java
@@ -0,0 +1,25 @@
+package com.thewayhome.ptis.service;
+
+import com.thewayhome.ptis.vo.wsbus.*;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import reactor.core.publisher.Mono;
+
+public class WsBusGetRoutePathListAPIServiceImpl extends AbstractWsBusAPIService {
+    public WsBusGetRoutePathListAPIServiceImpl(
+            @Value("${openapi.arrinfo.bus.endpoint}") String endpoint,
+            @Value("${openapi.data-gov.key.decoding}") String apiKey,
+            @Value("${openapi.arrinfo.bus.path.getroutepathlist}") String path
+    ) {
+        super(apiKey, endpoint, path, GetRoutePathListAPINrmRespVoImpl.class, CommAPIErrRespVoImpl.class);
+    }
+
+    public Mono<IServiceResult> getDataFromOpenAPI(@Valid GetRoutePathListAPIReqVo req) {
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+        queryParams.add("busRouteId", req.getBusRouteId());
+
+        return this.getDataFromOpenAPI(queryParams);
+    }
+}
