1a4cd24a012ad24ca002e2b07f3c151ba9b518ba
wasuphj <ninini6630@gmail.com>
2023-10-29 16:47:08 +0900

    [KHJ] B0002 작업용 파라미터 사용 - 코드 하드코딩 String 삭제

--- ptis-batch/src/main/java/com/thewayhome/ptis/batch/job/b0002/B0002DoMainLogicItemProcessor.java
+++ ptis-batch/src/main/java/com/thewayhome/ptis/batch/job/b0002/B0002DoMainLogicItemProcessor.java
@@ -1,11 +1,14 @@
 package com.thewayhome.ptis.batch.job.b0002;
 
+import com.thewayhome.ptis.batch.service.ParamService;
 import com.thewayhome.ptis.batch.util.APIConnector;
+import com.thewayhome.ptis.batch.vo.Param;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobInterruptedException;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.NoSuchJobException;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +18,7 @@ import org.springframework.util.MultiValueMap;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -24,12 +28,15 @@ public class B0002DoMainLogicItemProcessor implements ItemProcessor<B0002DoMainL
     private final String jobName;
     private final String jobDate;
     private StepExecution stepExecution;
+    private ParamService paramService;
     public B0002DoMainLogicItemProcessor(
             @Value("#{jobParameters[jobName]}") String jobName,
-            @Value("#{jobParameters[jobDate]}") String jobDate
+            @Value("#{jobParameters[jobDate]}") String jobDate,
+            ParamService paramService
     ) {
         this.jobName = jobName;
         this.jobDate = jobDate;
+        this.paramService = paramService;
     }
 
     @BeforeStep
@@ -45,14 +52,22 @@ public class B0002DoMainLogicItemProcessor implements ItemProcessor<B0002DoMainL
         final String arsId = input.getArsId();
         final List<B0002DoMainLogicItemOutputSub> busRouteList = new ArrayList<>();
 
+        Optional<Param> jobOptional = paramService.getBatchJobInputParam(jobName);
+        if (jobOptional.isEmpty()) {
+            throw new NoSuchJobException("No such Job Param exists. Jobname: [" + jobName + "]");
+        }
+
+        Param param = jobOptional.get();
+        String[] paramList = param.getValue().split("\\|");
+        int paramIdx = 0;
+
+        String endpoint = paramList[paramIdx++];
+        String path = paramList[paramIdx++];
+
         MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
         queryParams.add("arsId", arsId);
 
-        String dataFromAPI = APIConnector.getDataFromAPI(
-                "http://localhost:8082",
-                "/ws-pure/getRouteByStationList",
-                queryParams
-        ).block();
+        String dataFromAPI = APIConnector.getDataFromAPI(endpoint, path, queryParams).block();
 
         if (dataFromAPI == null) {
             throw new IllegalArgumentException();
