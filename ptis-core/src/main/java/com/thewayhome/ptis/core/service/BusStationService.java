71adbfcd605bcf6a22a3d80a8ba22ba411491609
wasuphj <ninini6630@gmail.com>
2023-10-29 17:17:28 +0900

    [KHJ] 버스 정류장 번호로 DB 조회 메소드 추가

--- ptis-core/src/main/java/com/thewayhome/ptis/core/service/BusStationService.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/service/BusStationService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BusStationService {
@@ -27,6 +28,10 @@ public class BusStationService {
         return busStationRepository.findAll();
     }
 
+
+    public Optional<BusStation> findByArsId(String arsId) {
+        return busStationRepository.findByBusStationId(arsId);
+    }
     public List<BusStation> findBusStationByGatheringStatusCode(String gatheringStatusCode, boolean notCondition) {
         List<BusStationProcess> busStationProcess = notCondition ?
                 busStationProcessRepository.findByGatheringStatusCodeNot(gatheringStatusCode) :
