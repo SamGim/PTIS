6832e4b960f7e0b37395228e35bac29480a74a96
jykim-rust <alexj99@naver.com>
2023-11-03 20:56:40 +0900

    [JYK] B0004배치의 Reader 구현

---
+++ ptis-batch/src/main/java/com/thewayhome/ptis/batch/job/b0004/B0004DoMainLogicItemReader.java
@@ -0,0 +1,144 @@
+package com.thewayhome.ptis.batch.job.b0004;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mysql.cj.util.StringUtils;
+import com.thewayhome.ptis.batch.job.b0001.B0001DoMainLogicItemInput;
+import com.thewayhome.ptis.batch.service.ParamService;
+import com.thewayhome.ptis.batch.util.CommonUtils;
+import com.thewayhome.ptis.batch.vo.Param;
+import com.thewayhome.ptis.core.service.BusRouteService;
+import com.thewayhome.ptis.core.vo.BusRoute;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobInterruptedException;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+@Slf4j
+@Component
+@Qualifier("B0004DoMainLogicItemReader")
+@StepScope
+public class B0004DoMainLogicItemReader implements ItemStreamReader<B0004DoMainLogicItemInput> {
+    private final String jobName;
+    private final String jobDate;
+    private final String type;
+    private final String filename;
+    private List<Map<String, Object>> jArray;
+    private int idx = 0;
+
+    public B0004DoMainLogicItemReader(
+            @Value("#{jobParameters[jobName]}") String jobName,
+            @Value("#{jobParameters[jobDate]}") String jobDate,
+            ParamService paramService
+    ) throws IndexOutOfBoundsException, NoSuchJobException, IOException {
+        this.jobName = jobName;
+        this.jobDate = jobDate;
+
+        Param param = paramService.getBatchJobInputParam(jobName)
+                .orElseThrow(() -> new NoSuchJobException("No such Job Param exists. Jobname: [" + jobName + "]"));
+
+        String[] paramList = param.getValue().split("\\|");
+        int paramIdx = 0;
+
+        this.type = paramList[paramIdx++];
+        this.filename = paramList[paramIdx++];
+
+        initialize();
+    }
+
+    public void initialize() throws IOException {
+        try {
+            ClassPathResource resource = new ClassPathResource(this.filename);
+            InputStream inputStream = resource.getInputStream();
+            String jString = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            this.jArray = getJsonArray(jString);
+        } catch (IOException e) {
+            // 파일 읽기 중 오류 발생
+            e.printStackTrace();
+            throw new IOException("File read error: " + this.filename);
+        }
+    }
+
+
+
+    private List<Map<String, Object>> getJsonArray(String jsonString) throws JsonProcessingException {
+        if ("J".equals(this.type)){
+            Map<String, Object> jsonMap = CommonUtils.convertJSONstringToMap(jsonString);
+            List<Map<String, Object>> jArray = (List<Map<String, Object>>) jsonMap.get("DATA");
+            if (jArray == null || jArray.size() == 0) {
+                throw new IllegalArgumentException("Invalid json string: " + jsonString);
+            }
+            return jArray;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid type: " + this.type);
+        }
+    }
+
+    @Override
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        this.idx = executionContext.getInt("READ_IDX", 0);
+        log.info("READ_IDX: " + this.idx);
+    }
+
+    @Override
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+        executionContext.put("READ_IDX", this.idx);
+        log.info("UPDATE_IDX : " + this.idx);
+    }
+
+    @Override
+    public void close() throws ItemStreamException {
+        log.info("CLOSE_IDX : " + this.idx);
+    }
+
+    @Override
+    public B0004DoMainLogicItemInput read() {
+        if (this.idx >= this.jArray.size()) {
+            return null;
+        }
+
+        Map<String, Object> item = this.jArray.get(this.idx++);
+
+        while (Objects.equals((String) item.get("dtlstatenm"), "폐업")){
+            if (this.idx >= this.jArray.size()) {
+                return null;
+            }
+            item = this.jArray.get(this.idx++);
+        }
+        String nodeId = (String) item.get("mgtno");
+        String nodeName = (String) item.get("bplcnm");
+        String nodeAddress = (String) item.get("sitewhladdr");
+        String nodePosX = (String) item.get("x");
+        String nodePosY = (String) item.get("y");
+        String nodeType = (String) item.get("uptaenm");
+
+        return B0004DoMainLogicItemInput
+                .builder()
+                .nodeId(nodeId)
+                .nodeAddress(nodeAddress)
+                .nodePosX(nodePosX)
+                .nodePosY(nodePosY)
+                .nodeType(nodeType)
+                .nodeName(nodeName)
+                .build();
+    }
+}
