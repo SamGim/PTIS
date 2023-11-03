5fdd8fe4aa86c300bafc5053d44940557a462441
jykim-rust <alexj99@naver.com>
2023-11-03 21:04:57 +0900

    [JYK] 음식점 및 카페 처리데이터를 저장할 JPARepository추가

---
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/repository/RestaurantProcessRepository.java
@@ -0,0 +1,7 @@
+package com.thewayhome.ptis.core.repository;
+
+import com.thewayhome.ptis.core.vo.RestaurantProcess;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantProcessRepository extends JpaRepository<RestaurantProcess, String> {
+}
