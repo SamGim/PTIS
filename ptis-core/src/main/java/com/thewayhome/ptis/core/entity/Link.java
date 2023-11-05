026ad66525e9f1299554cc402d2f65a76575d259
wasuphj <ninini6630@gmail.com>
2023-11-05 20:46:32 +0900

    [KHJ] Link Entity 생성

---
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/entity/Link.java
@@ -0,0 +1,44 @@
+package com.thewayhome.ptis.core.entity;
+
+import com.thewayhome.ptis.core.entity.base.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@Table(
+        name = "Link",
+        indexes = {
+                @Index(name = "Link_U1", columnList = "id"),
+                @Index(name = "Link_X1", columnList = "st_node, ed_node"),
+                @Index(name = "Link_X2", columnList = "link_type, link_name")
+        }
+)
+public class Link extends BaseEntity {
+    @Id
+    @Column(name="id", length = 12, nullable = false)
+    @Size(min = 12, max = 12)
+    private String id;
+
+    @Column(name="link_type", nullable = false)
+    @Size(max=10)
+    private String linkType;
+
+    @Column(name="link_name", nullable = false)
+    @Size(max=10)
+    private String linkName;
+
+    @Column(name="st_node", nullable = false)
+    private Node stNode;
+
+    @Column(name="ed_node", nullable = false)
+    private Node edNode;
+
+    @Column(name="cost", nullable = false)
+    private int cost;
+
+}
