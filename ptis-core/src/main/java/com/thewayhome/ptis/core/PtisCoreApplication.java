9c4c44900c6941de6cf00038b54a58881834d606
wasuphj <ninini6630@gmail.com>
2023-11-08 22:32:37 +0900

    [KHJ] Core를 DB 및 웹기능 비활성상태로 실행하도록 변경

--- ptis-core/src/main/java/com/thewayhome/ptis/core/PtisCoreApplication.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/PtisCoreApplication.java
@@ -1,13 +1,16 @@
 package com.thewayhome.ptis.core;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class PtisCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PtisCoreApplication.class, args);
+        new SpringApplicationBuilder(PtisCoreApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
     }
 
 }
