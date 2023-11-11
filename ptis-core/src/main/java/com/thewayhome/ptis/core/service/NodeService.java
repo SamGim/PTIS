f0d6bc2cce3d19e54ee3e0d529104d2557050869
wasuphj <ninini6630@gmail.com>
2023-11-11 22:12:02 +0900

    [KHJ] NodeService 내 BusStation → Node 변환(추가) 메소드 구현

--- ptis-core/src/main/java/com/thewayhome/ptis/core/service/NodeService.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/service/NodeService.java
@@ -3,6 +3,7 @@ package com.thewayhome.ptis.core.service;
 import com.thewayhome.ptis.core.dto.NodeRegisterReqDto;
 import com.thewayhome.ptis.core.entity.IdSequence;
 import com.thewayhome.ptis.core.entity.Node;
+import com.thewayhome.ptis.core.repository.BusStationProcessRepository;
 import com.thewayhome.ptis.core.repository.IdSequenceRepository;
 import com.thewayhome.ptis.core.repository.NodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,46 @@ public class NodeService {
     @Autowired
     private NodeRepository nodeRepository;
     @Autowired
+    private BusStationProcessRepository busStationProcessRepository;
+    @Autowired
     private IdSequenceRepository idSequenceRepository;
+
+    public Node createNodeFromBusStation(NodeRegisterReqDto req, String busStationId) {
+        Node node = new Node();
+
+        // ID
+        if (node.getId() == null) {
+            IdSequence idSequence = idSequenceRepository.findById("NODE")
+                    .orElse(new IdSequence("NODE", 0L));
+            Long nextId = idSequence.getNextId();
+
+            idSequence.setNextId(nextId + 1);
+            idSequenceRepository.save(idSequence);
+
+            node.setId(String.format("%012d", nextId + 1));
+
+            node.setCreatedAt(LocalDateTime.now());
+            node.setCreatedBy(req.getOperatorId());
+        }
+
+        // DATA
+        node.setNodeName(req.getNodeName());
+        node.setNodePosX(req.getNodePosX());
+        node.setNodePosY(req.getNodePosY());
+
+        // DB
+        node.setUpdatedAt(LocalDateTime.now());
+        node.setUpdatedBy(req.getOperatorId());
+
+        busStationProcessRepository.findById(busStationId)
+                .ifPresent(busStationProcess -> {
+                    busStationProcess.setNodeCreationStatusCode("01");
+                    busStationProcess.getNodes().add(node);
+                    busStationProcessRepository.save(busStationProcess);
+                });
+
+        return nodeRepository.save(node);
+    }
 
     public Node saveNode(NodeRegisterReqDto req) {
         Node node = new Node();
