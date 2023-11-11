11ca5f06355b8f1bf992464f5b5ff773306dab68
wasuphj <ninini6630@gmail.com>
2023-11-11 22:14:42 +0900

    [KHJ] NodeRepository 내 ID 범위로 Node Pair 조회 메소드 추가

--- ptis-core/src/main/java/com/thewayhome/ptis/core/repository/NodeRepository.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/repository/NodeRepository.java
@@ -2,8 +2,15 @@ package com.thewayhome.ptis.core.repository;
 
 import com.thewayhome.ptis.core.entity.Node;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NodeRepository extends JpaRepository<Node, String> {
+    @Query("SELECT o FROM Node o WHERE o.id >= :nodeIdSt AND o.id <= :nodeIdEd")
+    List<Node> findByIdsBetween(@Param("nodeIdSt") String nodeIdSt, @Param("nodeIdEd") String nodeIdEd);
+
 }
