d76694aca86e7a1d3a46cb3cbb5147fe826b319f
wasuphj <ninini6630@gmail.com>
2023-11-11 13:38:25 +0900

    [KHJ] BusRouteProcess 생성 시 상태코드 추가

--- ptis-core/src/main/java/com/thewayhome/ptis/core/service/BusRouteService.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/service/BusRouteService.java
@@ -81,6 +81,8 @@ public class BusRouteService {
             busRouteProcess.setCreatedBy(req.getOperatorId());
             busRouteProcess.setFirstGatheringDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")));
             busRouteProcess.setGatheringStatusCode("01");
+            busRouteProcess.setSelfGatheringStatusCode("01");
+            busRouteProcess.setStationGatheringStatusCode("00");
         }
         busRouteProcess.setUpdatedAt(LocalDateTime.now());
         busRouteProcess.setUpdatedBy(req.getOperatorId());
