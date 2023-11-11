02a53ce58a60cc3096cbf3c757edc191bf268b2a
wasuphj <ninini6630@gmail.com>
2023-11-11 17:15:04 +0900

    [KHJ] APIConnector 내 비동기/동기 로직 혼재 수정

--- ptis-batch/src/main/java/com/thewayhome/ptis/batch/util/APIConnector.java
+++ ptis-batch/src/main/java/com/thewayhome/ptis/batch/util/APIConnector.java
@@ -6,11 +6,13 @@ import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
@@ -58,9 +60,9 @@ public class APIConnector {
             if (response.statusCode().is2xxSuccessful()) {
                 return response.bodyToMono(String.class);
             } else if (response.statusCode().is4xxClientError()) {
-                return Mono.error(new RuntimeException("클라이언트 에러: " + response.statusCode()));
+                return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "클라이언트 에러: " + response.statusCode()));
             } else {
-                return Mono.error(new RuntimeException("서버 에러: " + response.statusCode()));
+                return Mono.error(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러: " + response.statusCode()));
             }
         });
     }
