a4a721ce424b824ae44aa2953a8baab2e497d00b
jykim-rust <alexj99@naver.com>
2023-11-12 15:35:23 +0900

    [JYK] B0006 배치작업 FinalizeJobTasklet 생성

---
+++ ptis-batch/src/main/java/com/thewayhome/ptis/batch/job/B0006/B0006FinalizeJobTasklet.java
@@ -0,0 +1,52 @@
+package com.thewayhome.ptis.batch.job.B0006;
+
+import com.thewayhome.ptis.batch.job.base.AbstractFinalizeJobTasklet;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobInterruptedException;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Qualifier("B0006FinalizeJobTasklet")
+@StepScope
+public class B0006FinalizeJobTasklet extends AbstractFinalizeJobTasklet {
+    private StepExecution stepExecution;
+
+    public B0006FinalizeJobTasklet(
+            @Value("#{jobParameters[jobName]}") String jobName,
+            @Value("#{jobParameters[jobDate]}") String jobDate
+    ) {
+        super(jobName, jobDate);
+    }
+
+    @BeforeStep
+    public void saveStepExecution(StepExecution stepExecution) {
+        this.stepExecution = stepExecution;
+    }
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws JobExecutionException {
+        if (this.stepExecution != null && this.stepExecution.isTerminateOnly()) {
+            throw new JobInterruptedException("Job is stopping");
+        }
+
+        String jobName = this.getJobName();
+        String jobDate = this.getJobDate();
+        String taskletName = this.getTaskletName();
+
+        log.info("[" + taskletName + "] jobName = " + jobName);
+        log.info("[" + taskletName + "] jobDate = " + jobDate);
+
+        return RepeatStatus.FINISHED;
+    }
+}
+
