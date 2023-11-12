6755fdc826047df407d52861a47f5473b7c25229
jykim-rust <alexj99@naver.com>
2023-11-12 15:34:48 +0900

    [JYK] B0006 배치작업 Input값을 Gym엔티티로 만들어주는 Writer 구현

---
+++ ptis-batch/src/main/java/com/thewayhome/ptis/batch/job/B0006/B0006DoMainLogicItemWriter.java
@@ -0,0 +1,52 @@
+package com.thewayhome.ptis.batch.job.B0006;
+
+
+import com.thewayhome.ptis.core.dto.GymRegisterReqDto;
+import com.thewayhome.ptis.core.service.GymService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Qualifier("B0006DoMainLogicItemWriter")
+@StepScope
+public class B0006DoMainLogicItemWriter implements ItemWriter<B0006DoMainLogicItemOutput> {
+
+    private final String jobName;
+    private final String jobDate;
+
+    private final GymService gymService;
+
+    public B0006DoMainLogicItemWriter(
+            @Value("#{jobParameters[jobName]}") String jobName,
+            @Value("#{jobParameters[jobDate]}") String jobDate,
+            GymService gymService
+    ) {
+        this.jobName = jobName;
+        this.jobDate = jobDate;
+        this.gymService = gymService;
+    }
+
+    @Override
+    public void write(Chunk<? extends B0006DoMainLogicItemOutput> chunk) throws Exception {
+        for (B0006DoMainLogicItemOutput item : chunk.getItems()) {
+            GymRegisterReqDto newEntity = GymRegisterReqDto.builder()
+                    .gymId(item.getNodeId())
+                    .gymAddress(item.getNodeAddress())
+                    .gymPosX(item.getNodePosX())
+                    .gymPosY(item.getNodePosY())
+                    .gymName(item.getNodeName())
+                    .build();
+
+            // DB
+            newEntity.setOperatorId(this.jobName);
+            gymService.saveGym(newEntity);
+        }
+
+    }
+}
