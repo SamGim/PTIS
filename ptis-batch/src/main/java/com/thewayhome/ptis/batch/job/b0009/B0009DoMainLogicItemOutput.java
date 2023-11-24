c1961c68b705c5b799825009acffa78bd1525d87
jykim-rust <alexj99@naver.com>
2023-11-24 16:51:49 +0900

    [JYK] B0009배치 작업을 위한 ItemOutput생성

---
+++ ptis-batch/src/main/java/com/thewayhome/ptis/batch/job/b0009/B0009DoMainLogicItemOutput.java
@@ -0,0 +1,12 @@
+package com.thewayhome.ptis.batch.job.b0009;
+
+import com.thewayhome.ptis.core.dto.request.NodeRegisterRequestDto;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class B0009DoMainLogicItemOutput {
+    private String companyId;
+    private NodeRegisterRequestDto nodeRegisterReqDto;
+}
