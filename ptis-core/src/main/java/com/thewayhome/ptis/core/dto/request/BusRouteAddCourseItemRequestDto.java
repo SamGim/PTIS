d2058946786b1cdd3f7fe7a4bef97dccf0241323
wasuphj <ninini6630@gmail.com>
2023-11-26 17:47:24 +0900

    [KHJ] 버스노선 코스 등록 DTO 정의

---
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/dto/request/BusRouteAddCourseItemRequestDto.java
@@ -0,0 +1,21 @@
+package com.thewayhome.ptis.core.dto.request;
+
+import com.thewayhome.ptis.core.dto.base.BaseDto;
+import com.thewayhome.ptis.core.vo.BusRouteVo;
+import com.thewayhome.ptis.core.vo.BusStationVo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+public class BusRouteAddCourseItemRequestDto extends BaseDto {
+    private String id;
+    private BusRouteVo busRoute;
+    private BusStationVo busStation;
+    private LocalDateTime firstBusTime;
+    private LocalDateTime lastBusTime;
+}
