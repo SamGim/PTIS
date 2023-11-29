9d51bda366348b381f7dc2ff1deab132339e9ed7
jykim-rust <alexj99@naver.com>
2023-11-29 19:35:46 +0900

    [JYK] B0014작업을 위한 메소드 추가

--- ptis-core/src/main/java/com/thewayhome/ptis/core/repository/LinkRepository.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/repository/LinkRepository.java
@@ -1,9 +1,16 @@
 package com.thewayhome.ptis.core.repository;
 
 import com.thewayhome.ptis.core.entity.Link;
+import com.thewayhome.ptis.core.entity.Node;
+import com.thewayhome.ptis.core.vo.NodeVo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LinkRepository extends JpaRepository<Link, String> {
+    Optional<Link> findByStNodeAndEdNodeAndLinkType(Node stNode, Node edNode, String linkType);
+
+    Optional<Link> findByStNodeAndEdNode(Node stNode, Node edNode);
 }
