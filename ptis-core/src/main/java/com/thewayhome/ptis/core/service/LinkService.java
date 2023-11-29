2ed9b7e20f51691b6273952c1bed9fa604be0137
jykim-rust <alexj99@naver.com>
2023-11-29 19:38:34 +0900

    [JYK] B0014작업을 위한 메소드 추가

--- ptis-core/src/main/java/com/thewayhome/ptis/core/service/LinkService.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/service/LinkService.java
@@ -1,5 +1,8 @@
 package com.thewayhome.ptis.core.service;
 
+import com.thewayhome.ptis.core.entity.Node;
+import com.thewayhome.ptis.core.util.NodeEntityVoConverter;
+import com.thewayhome.ptis.core.vo.BusStationVo;
 import com.thewayhome.ptis.core.vo.LinkVo;
 import com.thewayhome.ptis.core.dto.request.LinkRegisterRequestDto;
 import com.thewayhome.ptis.core.entity.IdSequence;
@@ -7,6 +10,7 @@ import com.thewayhome.ptis.core.entity.Link;
 import com.thewayhome.ptis.core.repository.IdSequenceRepository;
 import com.thewayhome.ptis.core.repository.LinkRepository;
 import com.thewayhome.ptis.core.util.LinkEntityVoConverter;
+import com.thewayhome.ptis.core.vo.NodeVo;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +23,7 @@ public class LinkService {
     private final LinkRepository linkRepository;
     private final IdSequenceRepository idSequenceRepository;
     private final LinkEntityVoConverter linkEntityDtoConverter;
+    private final NodeEntityVoConverter nodeEntityVoConverter;
     
     public Optional<Link> findById(String id) {
         return linkRepository.findById(id);
@@ -54,5 +59,28 @@ public class LinkService {
         Link link = this.saveLink(linkVo);
 
         return link;
+    }
+
+
+    public Link findBySourceNodeAndDestNode(NodeVo stNode, NodeVo edNode, String jobname) {
+        Node stNodeE = nodeEntityVoConverter.toEntity(stNode, jobname);
+        Node edNodeE = nodeEntityVoConverter.toEntity(edNode, jobname);
+        Optional<Link> link = linkRepository.findByStNodeAndEdNode(stNodeE, edNodeE);
+        // link가 null이면 새로 만든다.
+        if (link.isEmpty()) {
+            String linkName = String.format("%s-%s", stNode.getNodeName(), stNode.getNodeName());
+            LinkRegisterRequestDto req = LinkRegisterRequestDto.builder()
+                    .linkName(linkName)
+                    .linkType("B")
+                    .stNode(stNode)
+                    .edNode(edNode)
+                    .cost(Long.MAX_VALUE)
+                    .operatorId(jobname)
+                    .build();
+            return this.registerLink(req);
+        }
+        else {
+            return link.get();
+        }
     }
 }
