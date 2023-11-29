df878715af9b717b80788fb64d4833055ebe01c6
jykim-rust <alexj99@naver.com>
2023-11-29 19:34:16 +0900

    [JYK] B0014작업을 위한 메소드 추가

--- ptis-core/src/main/java/com/thewayhome/ptis/core/repository/BusRouteCourseRepository.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/repository/BusRouteCourseRepository.java
@@ -4,11 +4,14 @@ import com.thewayhome.ptis.core.entity.BusRouteCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public interface BusRouteCourseRepository extends JpaRepository<BusRouteCourse, String> {
     List<BusRouteCourse> findByBusRouteId(String busRouteId);
-    List<BusRouteCourse> findByBusStationId(String busStationId);
+    List<BusRouteCourse> findByBusStationIdAndFirstBusTimeIsNotNull(String busStationId);
     List<BusRouteCourse> findByBusRouteIdAndBusStationId(String busRouteId, String busStationId);
+    List<BusRouteCourse> findByFirstBusTimeIsNotNullAndBusRouteIdAndFirstBusTimeAfter(String busRouteId, LocalTime firstBusTime);
 }
