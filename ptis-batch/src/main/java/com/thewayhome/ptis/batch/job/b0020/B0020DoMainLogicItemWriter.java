892f7d060d80eb03ed7a9eac18abad657a2fdad8
jykim-rust <alexj99@naver.com>
2023-12-01 20:38:55 +0900

    [JYK] 시간이 업데이트된 SPL들만 저장

--- ptis-batch/src/main/java/com/thewayhome/ptis/batch/job/b0020/B0020DoMainLogicItemWriter.java
+++ ptis-batch/src/main/java/com/thewayhome/ptis/batch/job/b0020/B0020DoMainLogicItemWriter.java
@@ -1,10 +1,12 @@
 package com.thewayhome.ptis.batch.job.b0020;
 
-import com.thewayhome.ptis.batch.job.b0010.B0010DoMainLogicItemOutput;
+import com.thewayhome.ptis.batch.job.b0020.B0020DoMainLogicItemOutput;
 import com.thewayhome.ptis.core.dto.request.LinkRegisterRequestDto;
+import com.thewayhome.ptis.core.dto.request.ShortestPathLinkRegisterDto;
 import com.thewayhome.ptis.core.service.BusRouteService;
 import com.thewayhome.ptis.core.service.LinkService;
 import com.thewayhome.ptis.core.service.MessageService;
+import com.thewayhome.ptis.core.service.ShortestPathLinkService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobInterruptedException;
 import org.springframework.batch.core.StepExecution;
@@ -16,30 +18,32 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Slf4j
 @Component
-@Qualifier("B0010DoMainLogicItemWriter")
+@Qualifier("B0020DoMainLogicItemWriter")
 @StepScope
-public class B0010DoMainLogicItemWriter implements ItemWriter<B0010DoMainLogicItemOutput> {
+public class B0020DoMainLogicItemWriter implements ItemWriter<B0020DoMainLogicItemOutput> {
     private final String jobName;
     private final String jobDate;
     private StepExecution stepExecution;
     private final MessageService messageService;
     private final BusRouteService busRouteService;
-    private final LinkService linkService;
+    private final ShortestPathLinkService shortestPathLinkService;
 
-    public B0010DoMainLogicItemWriter(
+    public B0020DoMainLogicItemWriter(
             @Value("#{jobParameters[jobName]}") String jobName,
             @Value("#{jobParameters[jobDate]}") String jobDate,
             MessageService messageService,
             BusRouteService busRouteService,
-            LinkService linkService
+            ShortestPathLinkService shortestPathLinkService
     ) {
         this.jobName = jobName;
         this.jobDate = jobDate;
         this.messageService = messageService;
         this.busRouteService = busRouteService;
-        this.linkService = linkService;
+        this.shortestPathLinkService = shortestPathLinkService;
     }
 
     @BeforeStep
@@ -48,14 +52,13 @@ public class B0010DoMainLogicItemWriter implements ItemWriter<B0010DoMainLogicIt
     }
 
     @Override
-    public void write(Chunk<? extends B0010DoMainLogicItemOutput> chunk) throws Exception {
+    public void write(Chunk<? extends B0020DoMainLogicItemOutput> chunk) throws Exception {
         if (this.stepExecution != null && this.stepExecution.isTerminateOnly()) {
             throw new JobInterruptedException("Job is stopping");
         }
 
-        for (B0010DoMainLogicItemOutput item : chunk.getItems()) {
-            LinkRegisterRequestDto linkRegisterRequestDto = item.getLinkRegisterRequestDto();
-            linkService.registerLink(linkRegisterRequestDto);
+        for (B0020DoMainLogicItemOutput item : chunk.getItems()) {
+            item.getShortestPathLinkRegisterDtoList().forEach(shortestPathLinkService::registerShortestPathLink);
         }
     }
 }
