8d915441b71ace5299475c99e82a143ce4d54bbb
jykim-rust <alexj99@naver.com>
2023-12-18 13:40:06 +0900

    [JYK] SPL에 새로운 정보 추가를 위한 코드 추가

---
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/dto/request/SPLResponseDto.java
@@ -0,0 +1,19 @@
+package com.thewayhome.ptis.core.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class SPLResponseDto {
+    private String stNodeName;
+    private String stNodeSrcType;
+    private String edNodeName;
+    private String edNodeSrcType;
+    private String cost;
+    private String linkType;
+}
--- ptis-core/src/main/java/com/thewayhome/ptis/core/service/ShortestPathLinkService.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/service/ShortestPathLinkService.java
@@ -1,37 +1,36 @@
-//package com.thewayhome.ptis.core.service;
-//
-//import com.thewayhome.ptis.core.dto.request.ShortestPathLinkRegisterDto;
-//import com.thewayhome.ptis.core.entity.IdSequence;
-//import com.thewayhome.ptis.core.entity.Link;
-//import com.thewayhome.ptis.core.entity.Node;
-//import com.thewayhome.ptis.core.entity.ShortestPathLink;
-//import com.thewayhome.ptis.core.repository.IdSequenceRepository;
-//import com.thewayhome.ptis.core.repository.LinkRepository;
-//import com.thewayhome.ptis.core.repository.NodeRepository;
-//import com.thewayhome.ptis.core.repository.ShortestPathLinkRepository;
-//import com.thewayhome.ptis.core.util.NodeEntityVoConverter;
-////import com.thewayhome.ptis.core.util.ShortestPathLinkVoConverter;
-//import com.thewayhome.ptis.core.vo.NodeVo;
-//import com.thewayhome.ptis.core.vo.ShortestPathLinkVo;
-//import jakarta.transaction.Transactional;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Comparator;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//@Slf4j
-//@RequiredArgsConstructor
-//public class ShortestPathLinkService {
-//    private final ShortestPathLinkRepository shortestPathLinkRepository;
-//    private final NodeRepository nodeRepository;
-//    private final NodeEntityVoConverter nodeEntityVoConverter;
-////    private final ShortestPathLinkVoConverter shortestPathLinkVoConverter;
-//    private final LinkRepository linkRepository;
-//    private final IdSequenceRepository idSequenceRepository;
+package com.thewayhome.ptis.core.service;
+
+import com.thewayhome.ptis.core.dto.request.SPLResponseDto;
+import com.thewayhome.ptis.core.dto.request.ShortestPathLinkRegisterDto;
+import com.thewayhome.ptis.core.entity.IdSequence;
+import com.thewayhome.ptis.core.entity.Link;
+import com.thewayhome.ptis.core.entity.Node;
+import com.thewayhome.ptis.core.entity.ShortestPathLink;
+import com.thewayhome.ptis.core.repository.IdSequenceRepository;
+import com.thewayhome.ptis.core.repository.LinkRepository;
+import com.thewayhome.ptis.core.repository.NodeRepository;
+import com.thewayhome.ptis.core.repository.ShortestPathLinkRepository;
+import com.thewayhome.ptis.core.util.NodeEntityVoConverter;
+//import com.thewayhome.ptis.core.util.ShortestPathLinkVoConverter;
+import com.thewayhome.ptis.core.vo.NodeVo;
+import com.thewayhome.ptis.core.vo.ShortestPathLinkVo;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ShortestPathLinkService {
+    private final ShortestPathLinkRepository shortestPathLinkRepository;
+    private final NodeRepository nodeRepository;
+    private final NodeEntityVoConverter nodeEntityVoConverter;
+//    private final ShortestPathLinkVoConverter shortestPathLinkVoConverter;
+    private final LinkRepository linkRepository;
+    private final IdSequenceRepository idSequenceRepository;
 //
 //    public ShortestPathLinkVo getSPLBySrcNodeIdAndDestNodeId(NodeVo stNodeVo, NodeVo edNodeVo, String jobName) {
 //        Node stNode = nodeEntityVoConverter.toEntity(stNodeVo, jobName);
@@ -124,4 +123,61 @@
 //    private ShortestPathLink saveShortestPathLinkD(ShortestPathLink req, String operatorId) {
 //        return shortestPathLinkRepository.save(req);
 //    }
-//}
+
+    public List<Object> getSplListByStNodeAndEdNode(String stNodeId, String edNodeId) {
+        // 반환할 빈 res 리스트 생성
+        List<Object> res = new ArrayList<>();
+        ShortestPathLink curSpl = shortestPathLinkRepository.findByStNodeIdAndEdNodeId(stNodeId, edNodeId).orElseThrow(() -> new IllegalArgumentException("No SPL found"));
+        String prevNodeId = curSpl.getPrevNodeId();
+        if (prevNodeId.equals(curSpl.getStNodeId())) {
+            res.add(Arrays.asList(curSpl.getStNodeId(), curSpl.getEdNodeId(), curSpl.getCost(), curSpl.getLinkType()));
+            return res;
+        }
+        else {
+            // prevNodeid가 stNodeId가 아니라면
+            // stNode부터 prevNode까지의 SPL을 res에 추가하고, prevNode를 stNode로 바꾸고 stNode부터 edNode까지의 SPL을 다시 찾는다.
+            while (!prevNodeId.equals(curSpl.getStNodeId())) {
+                curSpl = shortestPathLinkRepository.findByStNodeIdAndEdNodeId(curSpl.getStNodeId(), curSpl.getPrevNodeId()).orElseThrow(() -> new IllegalArgumentException("No SPL found"));
+                res.add(Arrays.asList(curSpl.getStNodeId(), curSpl.getEdNodeId(), curSpl.getCost(), curSpl.getLinkType()));
+                stNodeId = curSpl.getPrevNodeId();
+            }
+        }
+        return res;
+    }
+    public ArrayList<SPLResponseDto> getSplByStNodeAndEdNodeByRecursive(String stNodeId, String edNodeId){
+        ArrayList<SPLResponseDto> res = new ArrayList<>();
+        ShortestPathLink curSpl = shortestPathLinkRepository.findByStNodeIdAndEdNodeId(stNodeId, edNodeId).orElseThrow(() -> new IllegalArgumentException("No SPL found " + stNodeId + " -> " + edNodeId));
+        String prevNodeId = curSpl.getPrevNodeId();
+        if (prevNodeId.equals(curSpl.getStNodeId())) {
+            Node stNode = nodeRepository.findById(stNodeId).orElseThrow(() -> new IllegalArgumentException("No Node found " + stNodeId));
+            Node edNode = nodeRepository.findById(edNodeId).orElseThrow(() -> new IllegalArgumentException("No Node found " + edNodeId));
+            res.add(
+                    SPLResponseDto.builder()
+                            .stNodeName(stNode.getNodeName())
+                            .stNodeSrcType(stNode.getNodeSrcType())
+                            .edNodeName(edNode.getNodeName())
+                            .edNodeSrcType(edNode.getNodeSrcType())
+                            .cost(curSpl.getCost().toString())
+                            .linkType(curSpl.getLinkType())
+                            .build()
+            );
+        }
+        else {
+            curSpl = shortestPathLinkRepository.findByStNodeIdAndEdNodeId(stNodeId, prevNodeId).orElseThrow(() -> new IllegalArgumentException("No SPL found " + stNodeId + " -> " + prevNodeId));
+            Node stNode = nodeRepository.findById(stNodeId).orElseThrow(() -> new IllegalArgumentException("No Node found " + stNodeId));
+            Node edNode = nodeRepository.findById(prevNodeId).orElseThrow(() -> new IllegalArgumentException("No Node found " + prevNodeId));
+            res.add(
+                    SPLResponseDto.builder()
+                            .stNodeName(stNode.getNodeName())
+                            .stNodeSrcType(stNode.getNodeSrcType())
+                            .edNodeName(edNode.getNodeName())
+                            .edNodeSrcType(edNode.getNodeSrcType())
+                            .cost(curSpl.getCost().toString())
+                            .linkType(curSpl.getLinkType())
+                            .build()
+            );
+            res.addAll(getSplByStNodeAndEdNodeByRecursive(curSpl.getEdNodeId(), edNodeId));
+        }
+        return res;
+    }
+}
