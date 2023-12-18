843a8be8e4b9718282bd9dbaf89c1384cce87d5d
jykim-rust <alexj99@naver.com>
2023-12-18 13:40:06 +0900

    [JYK] SPL에 새로운 정보 추가를 위한 코드 추가

--- ptis-core/src/main/java/com/thewayhome/ptis/core/repository/ShortestPathLinkRepository.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/repository/ShortestPathLinkRepository.java
@@ -3,7 +3,10 @@ package com.thewayhome.ptis.core.repository;
 import com.thewayhome.ptis.core.entity.ShortestPathLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShortestPathLinkRepository extends JpaRepository<ShortestPathLink, Long> {
+    Optional<ShortestPathLink> findByStNodeIdAndEdNodeId(String stNodeId, String edNodeId);
 
     // nativeQuery = true: JPA가 아닌 SQL을 사용할 때
 //    @Query(value = "SELECT * FROM shortest_path_link WHERE st_node_id = :stNodeId AND ed_node_id = :edNodeId", nativeQuery = true)
