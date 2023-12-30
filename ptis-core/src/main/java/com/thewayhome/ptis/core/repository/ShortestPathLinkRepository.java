7c8bf7395d5c574adbab89611e4dadbb8ac33f94
jykim-rust <alexj99@naver.com>
2023-12-30 12:23:41 +0900

    :sparkles: 시작노드와 끝노드타입으로 시간순정렬해서 가져오는 쿼리 추가

--- ptis-core/src/main/java/com/thewayhome/ptis/core/repository/ShortestPathLinkRepository.java
+++ ptis-core/src/main/java/com/thewayhome/ptis/core/repository/ShortestPathLinkRepository.java
@@ -3,10 +3,13 @@ package com.thewayhome.ptis.core.repository;
 import com.thewayhome.ptis.core.entity.ShortestPathLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ShortestPathLinkRepository extends JpaRepository<ShortestPathLink, Long> {
     Optional<ShortestPathLink> findByStNodeIdAndEdNodeId(String stNodeId, String edNodeId);
+
+    List<ShortestPathLink> findByStNodeIdAndEdNodeTypeOrderByCostAsc(String companyId, String cx);
 
     // nativeQuery = true: JPA가 아닌 SQL을 사용할 때
 //    @Query(value = "SELECT * FROM shortest_path_link WHERE st_node_id = :stNodeId AND ed_node_id = :edNodeId", nativeQuery = true)
